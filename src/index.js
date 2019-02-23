ce45ca74decd3593f2720a45eceaf01a1efaebcd
pedrobgodoy <pedrohenrique.godoy@Outlook.com>
2019-02-23 17:09:58 -0300

    Add / route

--- src/index.js
+++ src/index.js
@@ -6,22 +6,9 @@ const app = express();
 
 const server = require('http').Server(app);
 
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
+const jwtCheck = require('./middleware/jwtCheck.js')
 
-var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: "https://bittencourt.auth0.com/.well-known/jwks.json"
-    }),
-    audience: 'https://walletbittencourt.com/api',
-    issuer: "https://bittencourt.auth0.com/",
-    algorithms: ['RS256']
-});
-
-app.use(jwtCheck);
+//app.use(jwtCheck);
 
 mongoose.connect(
     'mongodb://financebase:financebase10@ds237955.mlab.com:37955/financeapp',
@@ -34,6 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-server.listen(3000, () => {
-    console.log('Server started on port https://localhost:3000');
-});
+server.listen(5000, () => {});
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 
 const TransactionController = require('./controllers/TransactionController');
 
+routes.get('/', function(req, res){
+    res.send('Hello World!')
+})
 routes.get('/transactions/:id', TransactionController.index);
 routes.post('/transactions', TransactionController.store);
 routes.delete('/transactions/:id', TransactionController.delete);
