1b1384126a8c5b72ada407c9ca33dcdb571fe195
pedrobgodoy <pedrohenrique.godoy@Outlook.com>
2019-03-05 13:18:52 -0300

    Now using jwtCheck

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const jwtCheck = require('./middleware/jwtCheck.js')
 
-//app.use(jwtCheck);
+app.use(jwtCheck);
 
 mongoose.connect(
     'mongodb://financebase:financebase10@ds237955.mlab.com:37955/financeapp',
