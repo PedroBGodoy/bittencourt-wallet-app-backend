8392c453f5709b32a5f48b1a1af722ab1fd44dd0
pedrobgodoy <pedrohenrique.godoy@Outlook.com>
2019-03-12 19:32:27 -0300

    Teste

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const routes = express.Router();
 
-const authMiddleware = require("./middlewares/auth");
+//const authMiddleware = require("./middlewares/auth");
 
-const TransactionController = require("./controllers/transactionController");
-const AuthController = require("./controllers/authController");
+//const TransactionController = require("./controllers/transactionController");
+// const AuthController = require("./controllers/authController");
 
 //PUBLIC ROUTES
-routes.post("/auth/register", AuthController.register);
-routes.post("/auth/authenticate", AuthController.authenticate);
+// routes.post("/auth/register", AuthController.register);
+// routes.post("/auth/authenticate", AuthController.authenticate);
 
-routes.use(authMiddleware);
+routes.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+//routes.use(authMiddleware);
 
 //PRIVATE ROUTES
-routes.get("/transactions", TransactionController.index);
-routes.post("/transactions", TransactionController.store);
-routes.delete("/transactions", TransactionController.delete);
-routes.put("/transactions", TransactionController.update);
+// routes.get("/transactions", TransactionController.index);
+// routes.post("/transactions", TransactionController.store);
+// routes.delete("/transactions", TransactionController.delete);
+// routes.put("/transactions", TransactionController.update);
 
 module.exports = routes;
